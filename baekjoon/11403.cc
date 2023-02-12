ab156f4032bd97b43cb590d829b4bc3b090ff93f
windopper <open0916@naver.com>
2023-02-12 10:42:44 +0000

    .

--- baekjoon/11403.cc
+++ baekjoon/11403.cc
@@ -8,19 +8,19 @@ int main() {
     
     cin >> N;
 
-    int arr[N][N];
+    int height[N][N];
 
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
-            cin >> arr[i][j];
+            cin >> height[i][j];
         }
     }
 
     for(int m=0; m<N; m++) {
         for(int s=0; s<N; s++) {
             for(int e=0; e<N; e++) {
-                if (arr[s][e] == 0 && arr[s][m] == 1 && arr[m][e] == 1) {
-                    arr[s][e] = 1;
+                if (height[s][e] == 0 && height[s][m] == 1 && height[m][e] == 1) {
+                    height[s][e] = 1;
                 }
             }
         }
@@ -28,7 +28,7 @@ int main() {
 
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
-            cout << arr[i][j] << " ";
+            cout << height[i][j] << " ";
         }
         cout << "\n";
     }
