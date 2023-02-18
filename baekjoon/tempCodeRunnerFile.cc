edbf1dc2e93dc9598f4ad187bfb9e309305a1a16
windopper <open0916@naver.com>
2023-02-18 07:40:46 +0000

    .

--- baekjoon/tempCodeRunnerFile.cc
+++ baekjoon/tempCodeRunnerFile.cc
@@ -1,1 +1,13 @@
-//weight[x] += max_find;
+     // if ( x != y ) {
+        //     if ( x < y ) {
+        //         parent[y] = x;
+        //     }
+        //     else {
+        //         parent[x] = y;
+        //     }
+        // }
+        // else {
+        //     cout << cnt;
+        //     closed = true;
+        //     break;
+        // }
