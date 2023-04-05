3b84ab38ba6888db7e12ad5ca6241cf3edc261cb
windopper <open0916@naver.com>
2023-04-05 13:54:04 +0000

    .

--- baekjoon/temp.cc
+++ baekjoon/temp.cc
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+int N, M;
+int arr[8];
+
+int solve(vector<int>& picked, int cnt, int r) {
+    if(cnt > r) return 0;
+    if(cnt == r) {
+        for(int i=0; i<picked.size(); i++) {
+            if(picked[i] == 1) cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return 0;
+    }
+
+    for(int i=0; i<N; i++) {
+        picked[i] = 1;
+        solve(picked, cnt + 1, r);
+        picked[i] = 0;
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int N; 
-    cin >> N;
-    int i = 666;
-    int cnt = 0;
-    while(1) {
-        string t = to_string(i);
-        for(int j=0; j<t.length()-2; j++) {
-            if(t[j] == '6' && t[j+1] == '6' && t[j+2] == '6') {
-                ++cnt;
-                if(cnt == N) {
-                    cout << i;
-                    return 0;
-                }
-                break;
-            }
-        }
-        i++;
-    }
+    cin >> N >> M;
+    for(int i=0; i<N; i++) cin >> arr[i];
+    sort(arr, arr+N);
+    vector<int> picked(N, 0);
+    solve(picked, 0, M);
 }
