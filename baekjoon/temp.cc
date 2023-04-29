ea3b16281c7fa34f031a9bde7c6381a99a1b8848
windopper <open0916@naver.com>
2023-04-29 07:19:23 +0000

    .

--- baekjoon/temp.cc
+++ baekjoon/temp.cc
@@ -2,32 +2,34 @@
 
 using namespace std;
 
+vector<int> solve(vector<int>& arr, int l, int r) {
+    vector<int> sorted(arr.size());
+    if(l < r) {
+        int m = (l + r) >> 1;
+        solve(arr, l, m);
+        solve(arr, m+1, r);
+        int s = l; int e = m+1;
+        for(int i=l; i<=r; i++) {
+            if(s <= m && arr[s] <= arr[e]) {
+                sorted[i] = arr[s++];
+            }
+            else {
+                sorted[i] = arr[e++];
+            }
+        }
+        for(int i=l; i<=r; i++) {
+            arr[i] = sorted[i];
+        }
+    }
+    return sorted;
+}
+
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int K;
-    cin >> K;
-    for(int i=1; i<=K; i++) {
-        int n;
-        cin >> n;
-        int maxV = 0;
-        int minV = 100;
-        int largestGap = 0;
-        int before = -1;
-        vector<int> arr;
-        while(n--) {
-            int t; cin >> t;
-            arr.push_back(t);
-        }
-        sort(arr.begin(), arr.end(), greater<>());
-        for(int t : arr) {
-            if(before != -1) {
-                largestGap = max(largestGap, abs(before - t));
-            }
-            before = t;
-            maxV = max(maxV, t);
-            minV = min(minV, t);
-        }
-        cout << "Class " << i << '\n';
-        cout << "Max " << maxV << ", Min " << minV << ", Largest gap " << largestGap << '\n';
-    }
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for(int i=0; i<N; i++) cin >> arr[i];
+    vector<int> sorted = solve(arr, 0, arr.size() - 1);
+    for(int cur : sorted) cout << cur << " ";
 }
