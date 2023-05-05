77b25a6e0966db8ee3735e86f1ff44081e7aeeed
windopper <open0916@naver.com>
2023-05-05 02:33:30 +0000

    .

---
+++ baekjoon/1144.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int N, M;
+int arr[10][10];
+
+int solve(int cur) {
+    int ret = 987654321;
+    
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+}
