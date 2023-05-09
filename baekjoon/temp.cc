e57511d6d1f629b35ec23a7149bc8f2dd64c4319
windopper <open0916@naver.com>
2023-05-09 11:44:11 +0000

    .

--- baekjoon/temp.cc
+++ baekjoon/temp.cc
@@ -3,56 +3,59 @@
 using namespace std;
 
 vector<vector<pair<int, int>>> g;
-vector<int> vis1;
-vector<int> vis2;
-unordered_map<int, bool> isGates;
+vector<int> vis;
+vector<bool> isGates(50001);
+vector<bool> isSummit(50001);
 int minVal = numeric_limits<int>::max();
+vector<int> answer(2, 987654321);
 
-int solve(int cur, int summit, bool done, int intensity) {
-    if(!done && isGates[cur]) {
-        done = true;
+void solve(vector<int>& gates) {
+    priority_queue<pair<int, int>> pq;
+    vis.assign(50001, 987654321);
+    for (int gate : gates) {
+        pq.push({0, gate});
+        vis[gate] = 0;
     }
-    if(done && cur == summit) {
-        return intensity;
+    while (!pq.empty()) {
+        int w = pq.top().first;
+        int cur = pq.top().second;
+        pq.pop();
+        if (minVal < w) {
+            break;
+        }
+        if (isSummit[cur]) {
+            if (minVal > w) {
+                minVal = w;
+                answer[0] = cur;
+                answer[1] = w;
+            } else if (minVal == w && answer[0] > cur) {
+                answer[0] = cur;
+            }
+        }
+        if (vis[cur] < w || isSummit[cur]) continue;
+        for (pair<int, int> next : g[cur]) {
+            int alt = max(w, next.second);
+            if (minVal < alt) continue;
+            if (vis[next.first] > alt) {
+                vis[next.first] = alt;
+                pq.push({alt, next.first});
+            }
+        }
     }
-
-    int ret = numeric_limits<int>::max();
-    for(pair<int ,int> next : g[cur]) {
-        if(isGates[next.first] && summit != next.first) continue;
-        if(!done && vis1[next.first]) continue;
-        if(done && vis2[next.first]) continue;
-        if(!done) vis1[next.first] = 1;
-        if(done) vis2[next.first] = 1;
-        if(minVal <= next.second) continue;
-        ret = min(ret, solve(next.first, summit, done, max(intensity, next.second)));
-        if(!done) vis1[next.first] = 0;
-        if(done) vis2[next.first] = 0;
-    }
-    return ret;
 }
 
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
-    g.resize(10001);
-    vis1.assign(10001, 0);
-    vis2.assign(10001, 0);
-    for(vector<int> p : paths) {
+    g.resize(50001);
+    for (vector<int> p : paths) {
         g[p[0]].push_back({p[1], p[2]});
         g[p[1]].push_back({p[0], p[2]});
     }
-
-    for(int gate : gates) {
+    for (int gate : gates) {
         isGates[gate] = true;
     }
-
-    vector<int> answer(2);
-    for (int s : summits) {
-        int val = solve(s, s, false, 0);
-        if (minVal > val) {
-            minVal = val;
-            answer[0] = s;
-            answer[1] = val;
-        }
-        cout << s << " " << val << '\n';
+    for (int summit : summits) {
+        isSummit[summit] = true;
     }
+    solve(gates);
     return answer;
 }
